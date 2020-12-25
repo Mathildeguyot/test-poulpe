93c4b263bd44d2fbf482c45f3eefede378c973c3
Mathilde Guyot <mathilde.guyot1@gmail.com>
2020-12-25 12:57:32 +0100

    ajouter un commentaire

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = Comment.new(comment_params)
+    @comment.task = @task
+    @comment.save
+    redirect_to tasks_path
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
