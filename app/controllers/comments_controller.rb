d22f3a3c410fd54b6cc5b01fb179ab8e961ac8cf
Mathilde Guyot <mathilde.guyot1@gmail.com>
2020-12-25 13:28:43 +0100

    supprimer un commentaire

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,12 @@ class CommentsController < ApplicationController
     redirect_to tasks_path
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to tasks_path
+  end
+
   def comment_params
     params.require(:comment).permit(:content)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :tasks do
     resources :comments, only: [:create]
   end
+
+  resources :comments, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
